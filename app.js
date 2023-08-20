c87e27199290f1124317cfbf72a2f4034bc8a802
mohamed khattab <mkhattab369@gmail.com>
2023-08-20 14:58:22 +0300

    starting the server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello, world");
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/db");
+const app = new express();
+let port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log("Server is listening on port " + port);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
--- todo.md
+++ todo.md
@@ -3,3 +3,12 @@
 ---
 
 1. Create basic structure
+2. start with basic connections
+
+- [x] create atlas collection
+- [x] basic db connection
+- [x] use connection string for MONGO_URL
+- [x] create basic server in app.js
+- [x] test if it is working
+
+3.
